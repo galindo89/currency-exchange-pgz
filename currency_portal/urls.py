11d99a39b85b6ebcd79912bc4eddf44b3b5a3049
Pablo <pablo@livedooh.com>
2024-12-20 05:12:01 +0100

    adding user app for pontential user customization

--- currency_portal/urls.py
+++ currency_portal/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('allauth.urls')),  # Allauth URLs
+    path('accounts/', include('allauth.urls')),  # Allauth URLs
 ]
