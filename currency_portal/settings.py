80eeffed359f772a1b9d59d6f6a49e3433f61a5b
Pablo <pablo@livedooh.com>
2024-12-30 15:24:39 +0100

    adding context process to display lates exchnage rate on nav bar

--- currency_portal/settings.py
+++ currency_portal/settings.py
@@ -129,6 +129,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "offers.context_processors.exchange_rate_processor"
             ],
         },
     },
---
+++ offers/context_processors.py
@@ -0,0 +1,20 @@
+from .models import LatestExchangeRate
+from .utils import fetch_and_update_exchange_rate
+
+def exchange_rate_processor(request):
+    try:
+        latest_rate = LatestExchangeRate.objects.latest("timestamp").rate
+        print(f"Latest exchange rate fetched from database: {latest_rate}")
+    except LatestExchangeRate.DoesNotExist:
+        try:
+            # If no rate exists in the database, fetch it from the API from openexchangerates.org
+            latest_rate = fetch_and_update_exchange_rate()
+            print(f"Latest exchange rate fetched: {latest_rate}")
+        except ValueError as e:
+            # Handle API errors gracefully and fallback to "N/A". Not desired. Let's check
+            print(f"Error fetching exchange rate: {e}")
+            latest_rate = "N/A"
+
+    return {
+        "exchange_rate": latest_rate
+    }
