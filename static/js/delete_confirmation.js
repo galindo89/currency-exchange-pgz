77e6a83e9bc4d52b0af630162ef13d24ed9c2e3c
Pablo <pablo@livedooh.com>
2025-01-09 08:59:39 +0100

    adding delete button to bid section and making modal reusable to confirm deleting of bid

--- static/js/delete_confirmation.js
+++ static/js/delete_confirmation.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const deleteModal = document.getElementById("deleteConfirmationModal");
-    const deleteForm = document.getElementById("deleteOfferForm");
+    const deleteForm = document.getElementById("deleteForm");
 
     deleteModal.addEventListener("show.bs.modal", function (event) {
-        
-        const button = event.relatedTarget;
-        
-        const action = button.getAttribute("data-action");
-        
-        deleteForm.setAttribute("action", action);
+        const button = event.relatedTarget; 
+        const actionUrl = button.getAttribute("data-action"); 
+        deleteForm.setAttribute("action", actionUrl); 
     });
 });
