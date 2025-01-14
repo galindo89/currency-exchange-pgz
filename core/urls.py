e613b51506d8ccd5c89b9dff5fd102b20b2a7b6f
Pablo <pablo@livedooh.com>
2025-01-14 01:12:46 +0100

    improving dashboard section of bids by adding a view  contact button

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home_redirect, dashboard
+from .views import home_redirect, dashboard,view_contact_details
 
 urlpatterns = [
     path('', home_redirect, name='home'),  
     path('dashboard/', dashboard, name='dashboard'),
+    path('bids/<int:bid_id>/view-contact/', view_contact_details, name='view_contact_details'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from offers.models import Offer, LatestExchangeRate, Bid
@@ -19,3 +20,19 @@ def dashboard(request):
         'my_offers': my_offers,
         'my_bids': my_bids,
     })
+    
+@login_required
+def view_contact_details(request, bid_id):
+    bid = get_object_or_404(Bid, id=bid_id, user=request.user)
+
+   
+    if bid.status == 'ACCEPTED' and bid.contact_shared:
+        user = bid.offer.user
+        contact_details = {
+            'name': f"{user.first_name} {user.last_name}".strip() or "Not Provided",  
+            'email': user.email  
+        }
+        return JsonResponse({'success': True, 'contact_details': contact_details})
+
+    return JsonResponse({'success': False, 'message': 'Contact details not available.'})
+
